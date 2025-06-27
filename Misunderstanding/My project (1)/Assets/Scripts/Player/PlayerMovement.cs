142899328b55fb9ba9fae3527124ae3570f18f7c
pascal.heritier <pascalheritier>
2025-06-27 23:18:45 +0200

    Add motion to player

--- Misunderstanding/My project (1)/Assets/Scripts/Player/PlayerMovement.cs
+++ Misunderstanding/My project (1)/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,91 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+
+public enum PlayerMotion
+{
+    Vertical,
+    Horizontal
+}
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField]
+    private float flyingSpeed;   
+   
+    [SerializeField]
+    private PlayerMotion playerMotion;
 
+    private PlayerInput playerInput;
+    private Animator playerAnimator;
+    private Rigidbody2D playerBody;
+    private Vector2 motionDirection;
+    private void Awake()
+    {
+        TryGetComponent(out playerInput);
+        TryGetComponent(out playerBody);
+        TryGetComponent(out playerAnimator);
+        playerInput.onActionTriggered += context =>
+        {
+            if (context.action.name == InputActionConstants.Player.InputActionMove)
+                OnMove(context);
+        };
+    }
+
+    private void OnMove(CallbackContext context)
+    {
+        if (!enabled && !context.canceled) // always get key up, to stop motion even after disabling it
+            return;
+
+        if (context.canceled)
+            StopMoving();
+
+        Vector2 playerInput = context.ReadValue<Vector2>();
+        if (playerMotion == PlayerMotion.Horizontal && playerInput.x != 0)
+        {
+            motionDirection.x = playerInput.x;
+            motionDirection.y = 0;
+        }
+        else if (playerMotion == PlayerMotion.Vertical && playerInput.y != 0)
+        {
+            motionDirection.x = 0;
+            motionDirection.y = playerInput.y;
+        }
+        else
+        {
+            StopMoving();
+        }
+    }
+
+    private void StopMoving()
+    {
+        motionDirection = Vector2.zero;
+    }
+    private void Update()
+    {
+        // Calling run here allows continuous horizontal movement when holding the motion input
+        UpdateRunningBehaviour();
+    }
+
+    private void UpdateRunningBehaviour()
+    {
+        // Actual movement
+        playerBody.linearVelocity = new Vector2(motionDirection.x * flyingSpeed, motionDirection.y * flyingSpeed);
+
+        // Visual animations
+        //if (motionDirection.x != 0)
+        //{
+        //    // flip sprite left-right
+        //    if (motionDirection.x > 0)
+        //        transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        //    else
+        //        transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        //}
+        //else if (motionDirection.y > 0)
+        //{
+        //}
+        //else if (motionDirection.y < 0)
+        //{
+        //}
+    }
 }
