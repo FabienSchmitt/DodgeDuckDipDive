9361d39d4e2e3e09c0a228fb3b8e17b7dad4c1bb
pascal.heritier <pascalheritier>
2025-06-28 11:32:30 +0200

    Add introduction text in level0

--- Misunderstanding/My project (1)/Assets/Scripts/Level0Manager.cs
+++ Misunderstanding/My project (1)/Assets/Scripts/Level0Manager.cs
@@ -4,6 +4,26 @@ using static UnityEngine.InputSystem.Controls.AxisControl;
 
 public class Level0Manager : MonoBehaviour
 {
+
+    [SerializeField] DialogueManager dialogueManager;
+    private string[] GetLevelStartDialogue() => new string[]
+    {
+        "Poor me, migrating bird with no sense of where to go, with those crazy humans burning everything with their oversized egos and twisted minds.",
+        "Well anyways, let's find a refreshing spot where I can finally rest away from this hassle!",
+        "Keyboard: WASD | Xbox controller: Joystick"
+    };
+
+
+    private void Awake()
+    {
+        dialogueManager = FindFirstObjectByType<DialogueManager>(FindObjectsInactive.Include);
+    }
+
+    private void Start()
+    {
+        dialogueManager.ShowDialogue(GetLevelStartDialogue());
+    }
+
     private void Update()
     {
         // define level0 logic here
