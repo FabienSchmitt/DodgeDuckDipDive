b3e91a122a392dd883c053bfc68c351f93b5c7d4
pascal.heritier <pascalheritier>
2025-06-28 12:19:44 +0200

    Add dialogue when collisionning with encounter bird

--- Misunderstanding/My project (1)/Assets/Scripts/Player/PlayerCollision.cs
+++ Misunderstanding/My project (1)/Assets/Scripts/Player/PlayerCollision.cs
@@ -5,9 +5,11 @@ public class PlayerCollision : MonoBehaviour
 {
     [SerializeField] BoxCollider2D boxCollider;
     [SerializeField] UIManager uiManager;
+    [SerializeField] DialogueManager dialogueManager;
     private void Awake()
     {
         uiManager = GameObject.FindAnyObjectByType<UIManager>();
+        dialogueManager = FindFirstObjectByType<DialogueManager>(FindObjectsInactive.Include);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -16,6 +18,15 @@ public class PlayerCollision : MonoBehaviour
         if (collision.gameObject.tag == "Obstacle")
         {
             uiManager.ShowGameOver();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "EncounterBird")
+        {
+            var encounterBirdTextManager = collision.gameObject.GetComponent<EncounterBirdTextManager>();
+            dialogueManager.ShowDialogue(encounterBirdTextManager.GetDialogueMessage());
         }
     }
 }
