1f2a73e24582249d444074edf412290822e751d9
pascal.heritier <pascalheritier>
2025-06-28 16:48:53 +0200

    Adapt object spawner for vertical player motion

--- Misunderstanding/My project (1)/Assets/Scripts/SpawnedObject/MovingObjectSpawner.cs
+++ Misunderstanding/My project (1)/Assets/Scripts/SpawnedObject/MovingObjectSpawner.cs
@@ -6,8 +6,14 @@ public class MovingObjectSpawner : MonoBehaviour
     [SerializeField] bool playerGoingRight;
     [SerializeField] float spawnTime;
     [SerializeField] GameObject player;
+    PlayerMovement playerMovement;
 
     private float elapsedTime = 0f;
+
+    private void Awake()
+    {
+        playerMovement = player.GetComponent<PlayerMovement>();
+    }
 
     // Update is called once per frame
     public void Update()
@@ -23,11 +29,21 @@ public class MovingObjectSpawner : MonoBehaviour
 
     public void SpawnObject()
     {
-        var playerGoingRight = player.transform.localScale.x > 0;
-        float offset = playerGoingRight ? 1.1f : -1.1f;
-        Vector3 pos = new Vector3(Random.value + offset, Random.value, 10);
-        pos = Camera.main.ViewportToWorldPoint(pos);
-
+        Vector3 pos = Vector3.zero;
+        if (playerMovement.playerMotion == PlayerMotion.Vertical)
+        {
+            var playerGoingRight = player.transform.localScale.x > 0;
+            float offset = playerGoingRight ? 1.1f : -1.1f;
+            pos = new Vector3(Random.value + offset, Random.value, 10);
+            pos = Camera.main.ViewportToWorldPoint(pos);
+        }
+        else if(playerMovement.playerMotion == PlayerMotion.Horizontal)
+        {
+            var playerGoingUp = player.transform.localScale.y > 0;
+            float offset = playerGoingUp ? 1.1f : -1.1f;
+            pos = new Vector3(Random.value, Random.value + offset, 10);
+            pos = Camera.main.ViewportToWorldPoint(pos);
+        }
         var spawnedObject = Instantiate(objectPrefab, pos, Quaternion.identity);
         var spawnObjectBehavior = spawnedObject.GetComponent<SpawnObjectMovement>();
         spawnObjectBehavior.Initialize(player);
