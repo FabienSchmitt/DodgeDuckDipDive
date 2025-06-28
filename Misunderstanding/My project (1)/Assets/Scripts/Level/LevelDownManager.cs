03df8060c7aacfc84d30c73abae6bbe63300d4af
pascal.heritier <pascalheritier>
2025-06-28 23:34:14 +0200

    Added dialogue texts for level Down

--- Misunderstanding/My project (1)/Assets/Scripts/Level/LevelDownManager.cs
+++ Misunderstanding/My project (1)/Assets/Scripts/Level/LevelDownManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class LevelDownManager : MonoBehaviour
@@ -6,24 +7,62 @@ public class LevelDownManager : MonoBehaviour
     [SerializeField] GameObject[] spawners;
     [SerializeField] float maxDistance;
     [SerializeField] GameObject floor;
+    [SerializeField] TextMeshProUGUI victoryMessage;
 
+    private FloorTrigger floorTrigger;
     private float currentHeight;
     private float minHeight;
     private UIManager uiManager;
+    private DialogueManager dialogueManager;
+
+    private string[] GetLevelStartDialogue() => new string[]
+    {
+        "To think of it, I was never really happy up here, what if instead I could live under the earth? No human could ever bother me there!",
+        "Let's find out if I can actually find a peaceful cave of mine down there, and never again see this loathed and polluted sky."
+    };
+
+    private string GetLevelEndingMessage() => "Congratulations on becoming a mole and escaping the hard reality up there!";
 
     private void Awake()
     {
         currentHeight = player.transform.position.y;
         minHeight = currentHeight - maxDistance;
 
-        uiManager = GameObject.FindAnyObjectByType<UIManager>();
+        uiManager = FindAnyObjectByType<UIManager>();
+        dialogueManager = FindAnyObjectByType<DialogueManager>(FindObjectsInactive.Include);
         var movement = floor.GetComponent<SpawnObjectMovement>();
         movement.Initialize(player);
+        floorTrigger = FindAnyObjectByType<FloorTrigger>();
         floor.transform.position = new Vector2(floor.transform.position.x, minHeight);
         floor.gameObject.SetActive(true);
     }
 
-    void Update()
+    private void Start()
     {
+        dialogueManager.ShowDialogue(GetLevelStartDialogue());
+    }
+
+    private void Update()
+    {
+        if (floorTrigger.IsMole)
+        {
+            ManageGameEnding();
+        }
+    }
+
+    private void ManageGameEnding()
+    {
+        if (uiManager.IsGameEndingScreenShowing())
+            return;
+        if (uiManager.IsDialogeBoxShowing())
+            return; // wait until dialogbox is closed to show other screens
+        victoryMessage.text = GetLevelEndingMessage();
+        StartCoroutine(ShowGameEnding());
+    }
+
+    private System.Collections.IEnumerator ShowGameEnding()
+    {
+        yield return new WaitForSeconds(3f);
+        uiManager.ShowGameEnd(true);
     }
 }
--- Misunderstanding/My project (1)/Assets/Scripts/SpawnedObject/FloorTrigger.cs
+++ Misunderstanding/My project (1)/Assets/Scripts/SpawnedObject/FloorTrigger.cs
@@ -5,12 +5,15 @@ public class FloorTrigger : MonoBehaviour
 {
     [SerializeField] GameObject player;
     private UIManager uiManager;
-    private bool isMole = false;
+    private DialogueManager dialogueManager;
+
+    public bool IsMole { get; private set; }
 
 
     private void Awake()
     {
         uiManager = FindAnyObjectByType<UIManager>();
+        dialogueManager= FindAnyObjectByType<DialogueManager>(FindObjectsInactive.Include);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -26,13 +29,15 @@ public class FloorTrigger : MonoBehaviour
 
         if (collision.gameObject.tag == "Player")
         {
-            if (!isMole)
+            if (!IsMole)
             {
+                IsMole = true;
                 var playerAnimator = player.GetComponent<Animator>();
                 var newScale = player.transform.localScale;
                 newScale.y = Math.Abs(newScale.y);
                 playerAnimator.SetBool("becomeMole", true);
                 player.transform.localScale = newScale;
+                dialogueManager.ShowDialogue(new string[] { "Turns out I have always been a mole all that time, who would have believed it? I am so glad I could dig this out, what an adventure it has been." });
             }
         }
     }
