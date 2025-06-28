560b6ae8aefd1c898e05f425a35051d15490f26e
pascal.heritier <pascalheritier>
2025-06-28 12:03:52 +0200

    Add flaming prefab with random bubble texts

---
+++ Misunderstanding/My project (1)/Assets/Scripts/SpawnedObject/EncounterBirdBubbleTextManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class EncounterBirdBubbleTextManager : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI bubble;
+
+    private string[] bubbleTextCandidates = new string[]
+    {
+        "Go the other way!",
+        "This is not the way!",
+        "Don't go there",
+        "WTF you doing?",
+        "Where are you going?",
+        "Flee you fool",
+        "Aaaaaaaarrrrghhhhhh!",
+        "Don't trust anyone"
+    };
+
+
+    private void Awake()
+    {
+        var random = new System.Random();
+        bubble.text = bubbleTextCandidates[random.Next(bubbleTextCandidates.Count())];
+    }
+}
